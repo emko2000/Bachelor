1139fc9b32395199b61f437692e0fa8e4d3d175a
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2022-06-01 10:08:51 +0200

    add method to generate demand

---
+++ src/main/java/bachelor/DemandGeneration.java
@@ -0,0 +1,40 @@
+package bachelor;
+
+import java.nio.file.Path;
+
+import org.matsim.freightDemandGeneration.FreightDemandGeneration;
+
+public class DemandGeneration {
+	public static void main(String[] args) {
+
+		Path output = Path.of("output/");
+		Path vehicleFilePath = Path.of("scenarios/freightDemandGeneration/testVehicleTypes.xml");
+		Path carrierCSVLocation = Path.of("scenarios/freightDemandGeneration/testCarrierCSV.csv");
+		Path demandCSVLocation = Path.of("scenarios/freightDemandGeneration/testDemandCSV.csv");
+		Path shapeFilePath = Path.of("scenarios/freightDemandGeneration/testShape/testShape.shp");
+		String populationLocation = "scenarios/freightDemandGeneration/testPopulation.xml";
+		String network = "https://raw.githubusercontent.com/matsim-org/matsim-libs/master/examples/scenarios/freight-chessboard-9x9/grid9x9.xml";
+		new FreightDemandGeneration().execute(
+				"--output", output.toString(),
+				"--carrierOption", "createCarriersFromCSV",
+				"--demandOption", "createDemandFromCSVAndUsePopulation",
+				"--populationOption", "usePopulationInShape",
+				"--populationSamplingOption", "createMoreLocations",
+				"--VRPSolutionsOption", "runJspritAndMATSim",
+				"--combineSimilarJobs", "false",
+				"--carrierFileLocation", "",
+				"--carrierVehicleFileLocation", vehicleFilePath.toString(),
+				"--shapeFileLocation", shapeFilePath.toString(),
+				"--shapeCRS", "WGS84",
+				"--populationFileLocation", populationLocation.toString(),
+				"--populationCRS", "WGS84",
+				"--network", network,
+				"--networkCRS", "WGS84",
+				"--networkChangeEvents", "",
+				"--inputCarrierCSV", carrierCSVLocation.toString(),
+				"--inputDemandCSV", demandCSVLocation.toString(),
+				"--populationSample", "0.5",
+				"--populationSamplingTo", "1.0",
+				"--defaultJspriIterations", "3");
+	}
+}
